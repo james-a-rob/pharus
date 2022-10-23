8ad4c96b1396a16ce1107ab9b5effb6eb72ff64e
James Robertson <jamesrobertson@James.Robertson's-MacBook-Pro>
2022-10-23 20:44:38 +0100

    more flash on icon + sound + demo demo gif

--- tray-ui.js
+++ tray-ui.js
@@ -1,15 +1,38 @@
 const { QSystemTrayIcon, QIcon } = require("@nodegui/nodegui");
+const { exec } = require('child_process');
+
 const { detectFaceTouch, setupFaceTouchAI } = require('./face-touch');
 const { startCamera } = require('./camera');
-const trayIcon = new QIcon(
-    "facepalm-icon-color-4.png")
+const trayIcon = new QIcon("facepalm-icon-color-4.png");
+
+const initFlashingIcon = () => {
+    let showFaded = false;
+    const trayIcon = new QIcon("facepalm-icon-color-4.png");
+    const trayIconFaded = new QIcon("facepalm-icon-color-4-faded.png");
+    const tray = new QSystemTrayIcon();
+    tray.setIcon(trayIcon);
+    setInterval(() => {
+        if (showFaded) {
+            tray.setIcon(trayIcon);
+            showFaded = false;
+
+        } else {
+            tray.setIcon(trayIconFaded);
+            showFaded = true;
+
+        }
+
+    }, 200);
+    return tray;
+
+
+}
 
 
 const init = async () => {
     const model = await setupFaceTouchAI();
-    const tray = new QSystemTrayIcon();
-    tray.setIcon(trayIcon);
 
+    const tray = initFlashingIcon();
     await startCamera();
     const stopped = false;
 
@@ -20,6 +43,9 @@ const init = async () => {
         const result = await detectFaceTouch(model);
 
         if (result) {
+            exec('afplay precussion.mp3')
+
+
             console.log('show')
             tray.show();
 
